a70285f4f813717b91af73259bb35fc93a6317fb
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2025-01-04 18:21:12 +0500

    commands added

--- front_end/src/screens/Home/Home.tsx
+++ front_end/src/screens/Home/Home.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react"
+import Logo from "../../assets/TheGram.png"
 const Home = () => {
     useEffect(()=>{
         document.body.classList.add("dark-theme")
@@ -6,7 +7,13 @@ const Home = () => {
     },[])
     return (
         <div>
-            <div className="left-sidebar absolute"></div>
+            <div className="left-sidebar absolute flex flex-col">
+                <div className="mt-[25px] box-content mb-[16px] mx-[12px]">
+                    <div className="w-[196px] opacity-100 active:opacity-70 hover:cursor-pointer">
+                    <img height={29} className="flex mt-2" width={103} src={Logo} alt="" />
+                    </div>
+                </div>
+            </div>
             <div className="outer-main-wrapper">
                 <div className="flex feed-wrapper justify-center items-center">
                     <div className="main-feed"></div>
